34050d0541205cfe784df870534f907fe18550a7
lcorcho <lcorcho@utb.edu.co>
2023-10-31 21:48:01 -0500

    NewHost

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ application = app
 msg  =''
 tipo =''
 
-
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://sql10658339:tI6ZDDAk7d@sql10.freesqldatabase.com/sql10658339'
 
 
 
@@ -299,3 +299,5 @@ def not_found(error):
     
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=10000)
+
+    
--- conexionBD.py
+++ conexionBD.py
@@ -4,19 +4,14 @@ import mysql.connector
 
 def connectionBD():
     mydb = mysql.connector.connect(
-        host ="dpg-cl0pl6gp2gis738ukop0-a",
-        port = "5432",
-        user ="mediacompanyusers",
-        passwd ="ZUJ5PNy4pRuqOSj2NC7pc6LVf9cEe5iX",
-        database = "mediacompany"
+        host ="sql10.freesqldatabase.com",
+        user ="sql10658339",
+        passwd ="tI6ZDDAk7d",
+        database = "sql10658339"
         )
     if mydb:
         print ("Conexion exitosa a BD")
         return mydb
     else:
         print("Error en la conexion a BD")
-    
-
-    
-    
     
