74d7179307b21a7d944e99e5861050483216edc3
lcorcho <lcorcho@utb.edu.co>
2023-11-02 13:31:47 -0500

    mariaalejandrayoli

--- app.py
+++ app.py
@@ -296,7 +296,8 @@ def not_found(error):
     
     
     
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000, debug=True)
+##if __name__ == '__main__':
+    ##app.run(host='0.0.0.0', port=10000, debug=True)
 
-    
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="10000", debug=True) #the IP address of the ix15 which is given by our lan router 
